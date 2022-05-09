ef34b383eaab74f112eb8e4f5818443f5f285a0a
Vansh <kachhwalvansh230@gmail.com>
2022-05-10 00:09:15 +0530

    Camera and Preview Page Added

--- lib/screens/camera_page.dart
+++ lib/screens/camera_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:vitals_ai/screens/video_page.dart';
+import 'package:vitals_ai/utils/routes.dart';
 
 class CameraPage extends StatefulWidget {
   const CameraPage({ Key? key }) : super(key: key);
@@ -11,6 +13,7 @@ class CameraPage extends StatefulWidget {
 class _CameraPageState extends State<CameraPage> {
 
   bool _isLoading = true;
+  bool _isRecording = true;
   late CameraController _cameraController;
 
   @override
@@ -24,12 +27,34 @@ class _CameraPageState extends State<CameraPage> {
     super.initState();
     _initCamera();
   }
+
   _initCamera() async {
     final cameras = await availableCameras();
     final front = cameras.firstWhere((camera) => camera.lensDirection == CameraLensDirection.front);
     _cameraController = CameraController(front, ResolutionPreset.max);
+    
     await _cameraController.initialize();
     setState(() => _isLoading = false);
+    await _cameraController.prepareForVideoRecording();
+    await _cameraController.startVideoRecording();
+    _recordVideo();
+  }
+
+  _recordVideo() async {
+    await Future.delayed(Duration(seconds: 5));
+    if(_isRecording) {
+      final file = await _cameraController.stopVideoRecording();
+      setState(() {
+        _isRecording = false;
+      });
+      // final route = MaterialPageRoute(
+      //   fullscreenDialog: true,
+      //   builder: (_) => VideoPage(filePath: file.path)
+      // );
+      Navigator.pushNamed(context, MyRoutes.homeRoute);
+    }else {
+      
+    }
   }
 
   @override
